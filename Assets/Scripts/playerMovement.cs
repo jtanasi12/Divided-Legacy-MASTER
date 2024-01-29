e98ad6c8741f2c77a4a01944de93ef17f80e6f0a
Joseph Tanasi <josephtanasi10@gmail.com>
2024-01-29 00:46:25 -0500

    Added movement, BUG issue with getting stuck, crouch isn't working yet

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,33 +6,52 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float speed;
-    Vector2 movement;
-
-    Rigidbody2D rigidBody;
     // Used to handle physics - uses a consistent speed of 0.002 seconds
     // and is frame-rate independant
+    [SerializeField]
+    private CharacterController2D controller;
+    [SerializeField]
+    private float runSpeed = 40f;
+    private float horizontalMove = 0f;
+    private bool jump = false;
+    private bool crouch = false;
+
 
     // Called before the first frame update
     void Start(){
-        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        // Get input Left & Right and A & D
+        // Left = -1, No movement = 0, Right = 1
+         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        
+        if(Input.GetButtonDown("Jump")){
+            jump = true;
+        }
+
+        // Keep crouch true while we hold the 's' button down 
+        // and only stop crouching when we let go of 's'
+        if(Input.GetButtonDown("Crouch")){
+            crouch = true;
+        }
+        else if(Input.GetButtonUp("Crouch")){
+            crouch = false;
+        }
     }
 
      // Frame rate independant, is calculated at every 0.002 seconds
     void FixedUpdate(){
 
-     rigidBody.MovePosition(rigidBody.position + (movement * speed * Time.deltaTime));
+        // Time.fixedDeltaTime has us move the same amount of time every frame which is 0.002 seconds. This makes it consistent across all plateforms
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+
+        // Ensures that we don't jump forever
+        jump = false;
 
    }
-   
-    // Update is called once per frame
-    void Update()
-    {
-        // Check for player movement once per. frame 
-        // Left = -1 No Key Press = 0 Right = 0
-        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        
-    }
    
 }
