d2a673c79adf227f098b0e1e85dc743d539fbc2f
Joseph Tanasi <josephtanasi10@gmail.com>
2024-03-05 16:48:51 -0500

    defaulted split to be off

--- Assets/Scripts/Characters/SwitchMechanic.cs
+++ Assets/Scripts/Characters/SwitchMechanic.cs
@@ -8,7 +8,12 @@ public class SwitchMechanic : MonoBehaviour
     public PlayableCharacters split;
     
     public bool player1Active = true;
-    
+
+    private void Awake()
+    {
+        split.enabled = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
