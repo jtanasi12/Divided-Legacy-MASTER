4a343afa1678e967502e9c516b076850429deed0
Sean Oushana <38673796+so3159@users.noreply.github.com>
2024-03-17 17:09:50 -0400

    Enemy Fireballs
    Can replace the asset from fireball to magic ball for Heaven enemies pretty easily, this was just to get the functionality down. Next step is in the arrow/fireball script to change the velocity to match direction

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Enemy : Characters
 {
+    public GameObject attackObjectPrefab;
+    public float coolDown = 2.5f;
+    private float timer;
+
     [SerializeField]
     private EnemyController enemyController;
 
@@ -13,18 +17,28 @@ public class Enemy : Characters
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = coolDown;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer -= Time.deltaTime;
+        if(timer <= 0){
+            shootProjectile();
+            timer = coolDown;
+        }
         enemyController.InputMechanics();
     }
 
   void OnCollisionEnter2D(Collision2D collision)
     {
         enemyDamage.DealDamage(collision);
+    }
+
+//virtual so that it can be realized with a different prefab
+    public virtual void shootProjectile(){
+        Instantiate(attackObjectPrefab, transform.position, Quaternion.identity);
     }
 
 }
