1a912d87d7e39af63c3170976504e39e96e4021d
Joseph Tanasi <josephtanasi10@gmail.com>
2024-03-21 20:37:41 -0400

    Minor Fix

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -10,14 +10,10 @@ public class Enemy : Characters
     [SerializeField]
     private EnemyDamage enemyDamage;
 
-    public GameObject attackObjectPrefab;
-    public float coolDown = 2.5f;
-    private float timer;
-
     // Start is called before the first frame update
     void Start()
     {
-        timer = coolDown;
+  
     }
 
     // Start is called before the first frame update
@@ -30,27 +26,9 @@ public class Enemy : Characters
     // Update is called once per frame
     void Update()
     {
-
-        timer -= Time.deltaTime;
-        if (timer <= 0)
-        {
-            ShootProjectile();
-            timer = coolDown;
-        }
-
         enemyController.InputMechanics();
     }
 
 
-    void OnCollisionStay2D(Collision2D collision)
-    {
-        enemyDamage.DealDamage(collision);
-    }
-
-    //virtual so that it can be realized with a different prefab
-    public virtual void ShootProjectile()
-    {
-        Instantiate(attackObjectPrefab, transform.position, Quaternion.identity);
-    }
 
 }
