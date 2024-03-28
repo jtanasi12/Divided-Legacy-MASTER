9b6c117e02c94e7aef6c627b687dd68553d9cda8
Joseph Tanasi <josephtanasi10@gmail.com>
2024-03-27 23:19:07 -0400

    Added a fail condition, restart game, added pause menu functionality

--- Assets/Scripts/Managers/GameState.cs
+++ Assets/Scripts/Managers/GameState.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class GameState : MonoBehaviour
 {
@@ -9,10 +10,18 @@ public class GameState : MonoBehaviour
   UnityEvent TogglePause;
   public GameObject pauseMenuUI;
 
+  [SerializeField]
+  private GameObject loseMenuUI;
+
+  [SerializeField]
+  private PlayerHealth cloudBoy;
+  [SerializeField]
+  private PlayerHealth split;
 
     private void Awake()
     {
         pauseMenuUI.SetActive(false);
+        loseMenuUI.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,9 +30,34 @@ public class GameState : MonoBehaviour
       if (sharedState.isPaused) Resume();
       else PauseGame();
     }
+
+     
   }
 
-  public void PauseGame(){
+    private void FixedUpdate()
+    {
+        if(cloudBoy.GetIsPlayerDead() || split.GetIsPlayerDead())
+        {
+            // ALlow the death animation to play before pausing
+            StartCoroutine(ActivateLoseMenuAfterDelay(1.5f));
+        }
+    }
+
+    IEnumerator ActivateLoseMenuAfterDelay(float delay)
+    {
+        // Wait for the specified delay
+        yield return new WaitForSeconds(delay);
+
+        // Activate the lose menu
+        loseMenuUI.SetActive(true);
+
+        // Pause the game
+        Time.timeScale = 0f;
+
+        Debug.Log("The player has died");
+    }
+
+    public void PauseGame(){
     pauseMenuUI.SetActive(true);
     Time.timeScale = 0f;
     sharedState.togglePause();
@@ -34,7 +68,18 @@ public class GameState : MonoBehaviour
     Time.timeScale = 1;
     sharedState.togglePause();
   }
-  public void GoToMainMenu(){
+
+    public void RetryGame()
+    {
+        Debug.Log("Restarting the game");
+        // Reload the scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1; // Unpause the game 
+        loseMenuUI.SetActive(false); // Ensure lose menu is deactivated
+
+    }
+
+    public void GoToMainMenu(){
     //This is where we should route to the main menu when we make one
   }
   public void QuitGame(){
