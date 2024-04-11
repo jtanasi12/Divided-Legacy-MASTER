89298187e3c2f557e668048a978545cfde8362e0
Joseph Tanasi <josephtanasi10@gmail.com>
2024-04-11 18:18:10 -0400

    Fixed Errors

--- Assets/Scripts/Characters/Player/Playable Characters/HeartPickUp.cs
+++ Assets/Scripts/Characters/Player/Playable Characters/HeartPickUp.cs
@@ -3,22 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HeartPickUp : Pickups
-
-    [SerializeField]
-    private PlayerHealth playerHealth;
-
- 
-
-    public bool GetSwitchedState()
-    {
-        return switchedState;
-    }
-
-    public void SetSwitchedState(bool state)
-    {
-        switchedState = state;
-    }
-
+{ 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
