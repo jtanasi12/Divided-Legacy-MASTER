1aa7358f68d22b49f58b1a0bfec12bdf17c448b5
Joseph Tanasi <josephtanasi10@gmail.com>
2024-04-17 21:49:46 -0400

    Fire ball sound effects added

--- Assets/Scripts/Characters/Player/Playable Characters/FireballRange.cs
+++ Assets/Scripts/Characters/Player/Playable Characters/FireballRange.cs
@@ -4,17 +4,40 @@ using UnityEngine;
 
 public class FireballRange : MonoBehaviour
 {
-    [SerializeField]
-    AudioSource fireballSoundFX;
+    private bool soundRange = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // If there is a fireball in range, we will cue the sound effect
-        // Should only play once because we are triggering the soundFX on entering the collision only
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Fireball"))
+        // Check if the entering collider is on the "Heart Collider" layer
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Heart Collider"))
         {
-            fireballSoundFX.Play(); 
+            soundRange = true;
+            Debug.Log("IN RANGE");
         }
+    }
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        // Check if the collider stays within the trigger area
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Heart Collider"))
+        {
+            soundRange = true;
+            Debug.Log("IN RANGE");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // Reset the state when the collider exits the trigger area
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Heart Collider"))
+        {
+            soundRange = false;
+            Debug.Log("Out Of RANGE");
+        }
+    }
+
+    public bool GetSoundRange()
+    {
+        return soundRange;
     }
 }
